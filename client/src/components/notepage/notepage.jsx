6f2bc1562c0698ee85a955634e77ebf3b3843262
OmarZalat <zaltomar18@gmail.com>
2024-05-06 21:29:22 +0300

    fetched user data in notepage and filtered according to email

--- client/src/components/notepage/notepage.jsx
+++ client/src/components/notepage/notepage.jsx
@@ -1,16 +1,42 @@
-import React, { useContext, useRef, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import styles from "./notepage.module.css";
 import { UserContext } from "../../userProvider";
 
 export default function Notepage() {
   const { userData } = useContext(UserContext);
   const [journalEntry, setJournalEntry] = useState("");
+  const [userId, setUserId] = useState(null);
+
+  useEffect(() => {
+    // Fetch user data to get user_id based on email
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch(`http://127.0.0.1:5000/api/users`);
+        const data = await response.json();
+        console.log(`userdata email is ${userData.email}`);
+        console.log(`data is ${data}`);
+        const user = data.find((user) => user.email === userData.email);
+        if (user) {
+          setUserId(user.user_id);
+          console.log(`user id has been set successfuly`);
+        } else {
+          console.error("User not found for email:", userData.email);
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    if (userData && userData.email) {
+      fetchUserData();
+    }
+  }, [userData]);
 
   const handleChange = (e) => {
     setJournalEntry(e.target.value);
   };
 
-  console.log(userData);
+  console.log(userId);
   return (
     <>
       <div className={styles.journal}>
